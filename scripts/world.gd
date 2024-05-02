2c5597b8e373e290756ea03d8d2a687bc8cf7bde
Ninjdai <ninjdai@duck.com>
2024-05-02 12:28:52 +0200

    Add Y sorting to tilemap

--- scripts/world.gd
+++ scripts/world.gd
@@ -4,7 +4,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var scene = load("res://scenes/chunks/map_foret_eveil.tscn").instantiate()
+	var scene = load("res://scenes/chunks/map_test.tscn").instantiate()
 	maps.add_child(scene)
 	pass # Replace with function body.
 
