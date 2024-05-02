66bb8d5d55f970931eb4c82164096ce28a72ba82
Ninjdai <ninjdai@duck.com>
2024-05-02 09:16:07 +0200

    add player node

---
+++ scripts/world.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+@onready var maps = $Maps
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var scene = load("res://scenes/chunks/map_foret_eveil.tscn").instantiate()
+	maps.add_child(scene)
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
