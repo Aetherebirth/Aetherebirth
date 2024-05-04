dca43e1f824f73eb4d3a2e58642cb661c88d0e01
Ninjdai <ninjdai@duck.com>
2024-05-04 15:30:53 +0200

    fix: server connection settings

--- main/connection.gd
+++ main/connection.gd
@@ -13,7 +13,9 @@ static var is_peer_connected: bool
 
 
 func _ready() -> void:
-	if Connection.is_server(): start_server(default_port)
+	var args = parse_cmdline_args()
+	if Connection.is_server():
+		start_server(args)
 	connected.connect(func(): Connection.is_peer_connected = true)
 	disconnected.connect(func(): Connection.is_peer_connected = false)
 	disconnected.connect(disconnect_all)
@@ -22,19 +24,34 @@ func _ready() -> void:
 static func is_server() -> bool:
 	return "--server" in OS.get_cmdline_args()
 
+func parse_cmdline_args() -> Dictionary:
+	var cmdline_args = OS.get_cmdline_args()
+	var args = {}
+	
+	var i: int = 0
+	while(i<len(cmdline_args)):
+		if cmdline_args[i]=="--server":
+			args.server = true
+		elif cmdline_args[i]=="--server-port":
+			args.port = int(cmdline_args[i+1])
+			i+=1
+		i+=1
+	if not "port" in args:
+		args.port = default_port
+	return args
 
-func start_server(port) -> void:
+func start_server(args: Dictionary) -> void:
 	if max_clients == 0:
 		max_clients = 32
 	
 	var peer = ENetMultiplayerPeer.new()
-	var err = peer.create_server(port, max_clients)
+	var err = peer.create_server(args.port, max_clients)
 	if err != OK:
 		print("Cannot start server. Err: " + str(err))
 		disconnected.emit()
 		return
 	else:
-		print("Server started")
+		print("Server started on port " + str(args.port))
 		connected.emit()
 	
 	multiplayer.multiplayer_peer = peer
