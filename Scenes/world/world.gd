1d22572fdda418a93b01084e31210230531e4ff8
Ninjdai <ninjdai@duck.com>
2024-05-08 19:57:20 +0200

    feat: position extrapolation

--- Scenes/world/world.gd
+++ Scenes/world/world.gd
@@ -33,6 +33,7 @@ func SpawnNewPlayer(player_id, _position):
 
 func DespawnPlayer(player_id):
 	print("Player %d despawned" % player_id)
+	await get_tree().create_timer(0.2).timeout
 	get_node("Players/%d" % player_id).queue_free()
 
 func UpdateWorldState(world_state):
@@ -43,23 +44,40 @@ func UpdateWorldState(world_state):
 func _physics_process(_delta):
 	var render_time = int(Time.get_unix_time_from_system()*1000) - interpolation_offset
 	if(world_state_buffer.size()>1):
-		while world_state_buffer.size() > 2 and render_time > world_state_buffer[1].T:
+		while world_state_buffer.size() > 2 and render_time > world_state_buffer[2].T:
 			world_state_buffer.pop_front()
-		var interpolation_factor = float(
-			render_time - world_state_buffer[0].T
-		) / float(world_state_buffer[1].T - world_state_buffer[0].T)
-		for player_id in world_state_buffer[1].keys():
-			if str(player_id)=="T":
-				continue
-			if player_id == multiplayer.get_unique_id():
-				continue
-			if not world_state_buffer[0].has(player_id):
-				continue
-			if get_node("Players").has_node(str(player_id)):
-				var new_position = lerp(world_state_buffer[0][player_id]["P"], world_state_buffer[1][player_id]["P"], interpolation_factor)
-				get_node("Players/%d" % player_id).MovePlayer(new_position)
-			else:
-				print("Spawning player %d"%player_id)
-				SpawnNewPlayer(player_id, world_state_buffer[1][player_id]["P"])
+		if(world_state_buffer.size()>2): # If we have a future state
+			var interpolation_factor = float(
+				render_time - world_state_buffer[1].T
+			) / float(world_state_buffer[2].T - world_state_buffer[1].T)
+			for player_id in world_state_buffer[2].keys():
+				if str(player_id)=="T":
+					continue
+				if player_id == multiplayer.get_unique_id():
+					continue
+				if not world_state_buffer[1].has(player_id):
+					continue
+				if get_node("Players").has_node(str(player_id)):
+					var new_position = lerp(world_state_buffer[1][player_id].P, world_state_buffer[2][player_id].P, interpolation_factor)
+					get_node("Players/%d" % player_id).MovePlayer(new_position)
+				else:
+					print("Spawning player %d"%player_id)
+					SpawnNewPlayer(player_id, world_state_buffer[2][player_id].P)
+		elif(render_time > world_state_buffer[1].T): # We have no future world state
+			var extrapolation_factor = float(
+				render_time - world_state_buffer[0].T
+			) / float(world_state_buffer[1].T - world_state_buffer[0].T) - 1.00
+			for player_id in  world_state_buffer[1].keys():
+				if str(player_id)=="T":
+					continue
+				if player_id == multiplayer.get_unique_id():
+					continue
+				if not world_state_buffer[0].has(player_id):
+					continue
+				if get_node("Players").has_node(str(player_id)):
+					var position_delta = (world_state_buffer[1][player_id].P - world_state_buffer[0][player_id].P)
+					var new_position = world_state_buffer[1][player_id].P + (position_delta*extrapolation_factor)
+					get_node("Players/%d" % player_id).MovePlayer(new_position)
+				
 			
 
